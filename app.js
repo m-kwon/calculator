ae2e98d8849d459de079ceace2262abb97b29ac6
Matthew Kwon <matthew.o.kwon@gmail.com>
2022-08-18 15:45:58 -0700

    Created basic operators and a operator router

---
+++ app.js
@@ -0,0 +1,22 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let operate = (operator, a, b) => {
+  a = Number(a);
+  b = Number(b);
+
+  switch(operator) {
+    case '+':
+      return add(a, b)
+    case '-':
+      return subtract(a, b)
+    case 'x':
+      return multiply(a, b)
+    case '÷':
+      return divide(a, b)
+    default:
+      return null;
+  }
+}
